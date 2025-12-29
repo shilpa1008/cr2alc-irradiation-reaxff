45cf1543e273c99400a3d01e7238394266668079
shilpa1008 <1008shilpasingh@gmail.com>
2025-12-29 15:21:13 +0530

    Update convert_to_qe.py

--- convert_to_qe.py
+++ convert_to_qe.py
@@ -1,3 +1,3 @@
 from ase.io import read, write
-structure = read("../../data/structures/initial/Cr2AlC.cif")
-write("../../data/structures/initial/Cr2AlC.xyz", structure)
+structure = read("../../data/structures/initial/cr2alc.cif")
+write("../../data/structures/initial/cr2alc.xyz", structure)
